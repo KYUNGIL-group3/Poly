b4c75491a7f74c4261af051816be9a3c2c0dcff4
kimkwangsik <jk140502@gmail.com>
2016-07-06 14:32:50 +0900

    적이동 로직 버그 수정 적캐릭터 구조 변경 공격시 피격 판정 수정 적애니메이션 추가(적용X, 작동방법 확인X)

--- Assets/02.Scripts/csEnemyRig.cs
+++ Assets/02.Scripts/csEnemyRig.cs
@@ -16,6 +16,8 @@ public class csEnemyRig : MonoBehaviour {
     STATE state = STATE.IDLE;
     float stateTime = 0.0f;
 
+	public GameObject motion;
+
     public float idleStateMaxTime = 1.0f;   //대기시간,경직시간
     public float attackableRange = 1.5f;    //공격범위
     public float attackStateMaxTime = 1.0f; //공격대기시간
@@ -29,102 +31,103 @@ public class csEnemyRig : MonoBehaviour {
 	private float distance;
 	Transform target;
 	CharacterController enemyController;
-    public int mHp = 100;
+	//Animator anim;
+    public int mHp = 300;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("MoveSkillObj").transform;
 		enemyController = GetComponent<CharacterController> ();
-        monsterAttackPoint = 10;
+		//anim = motion.GetComponent<Animator> ();
+
+
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(state);
-        switch (state)
-        {
-            case STATE.IDLE:
-                stateTime += Time.deltaTime;
-                if (stateTime > idleStateMaxTime)
-                {
-                    distance = Vector3.Distance(transform.position, player.position);
-                    stateTime = 0.0f;
-                    state = STATE.MOVE;
-                    if (distance < attackableRange) 
-                    {
-                        state = STATE.ATTACK;
-                    }
-                    if (distance < checkAttackDistance)
-                    {
-                        state = STATE.CHECKATTACK;
-                    }
-                }
+		//Debug.Log (state);
+		switch (state) {
+		case STATE.IDLE:
+			stateTime += Time.deltaTime;
+			if (stateTime > idleStateMaxTime) {
+				distance = Vector3.Distance (transform.position, player.position);
+				stateTime = 0.0f;
+				if (distance < attackableRange) {
+					//anim.SetBool ("enemyIsAttack", true);
+					state = STATE.ATTACK;
+				} else {
+					state = STATE.MOVE;
+				}
+
+				if (distance < checkAttackDistance) {
+					state = STATE.CHECKATTACK;
+				}
+			}
 
 
-                break;
-            case STATE.AFTERIDLE:
-                stateTime += Time.deltaTime;
-                if (stateTime > idleStateMaxTime)
-                {
-                    stateTime = 0.0f;
-                    state = STATE.IDLE;
-                }
-                break;
-            case STATE.MOVE:
+			break;
+		case STATE.AFTERIDLE:
+			stateTime += Time.deltaTime;
+			if (stateTime > idleStateMaxTime) {
+				stateTime = 0.0f;
+				state = STATE.IDLE;
+			}
+			break;
+		case STATE.MOVE:
                 //해당 스크립트 오브젝트와 player간의 거리
-                distance = Vector3.Distance(transform.position, player.position);
+			distance = Vector3.Distance (transform.position, player.position);
 
-                if (distance < attackableRange)
-                {
-                    state = STATE.ATTACK;
-                    stateTime = attackStateMaxTime;
-                }
-                    
-                else {
-                    Vector3 dir = player.position - transform.position;
-                    dir.y = 0.0f;
-                    dir.Normalize();
-                    enemyController.SimpleMove(dir * speed);
-                }
-                break;
-            case STATE.ATTACK:
-                stateTime += Time.deltaTime;                                                                            
-                if (stateTime > attackStateMaxTime)
-                {
-                    stateTime = 0.0f;
-                    GameManager.Instance().PlayerHealth(monsterAttackPoint);
-                }
-                state = STATE.IDLE;
-                break;
-            case STATE.DAMAGE:
-                stateTime = 0.0f;
+			if (distance < attackableRange) {
+				state = STATE.IDLE;
+				//stateTime = attackStateMaxTime;
+				stateTime = 0.0f;
+			} else {
+				Vector3 dir = player.position - transform.position;
+				dir.y = 0.0f;
+				dir.Normalize ();
+				enemyController.SimpleMove (dir * speed);
+			}
+			break;
+		case STATE.ATTACK:
+			stateTime += Time.deltaTime;                                                                            
+			if (stateTime > attackStateMaxTime) {
+				stateTime = 0.0f;
+				GameManager.Instance ().PlayerHealth (monsterAttackPoint);
+				Debug.Log("Attack");
+				//anim.SetBool ("enemyIsAttack", false);
+				state = STATE.IDLE;
+			}
+			break;
+		case STATE.DAMAGE:
+
+			if (mHp <= 0) {
+				state = STATE.DEAD;
+				GameManager.Instance ().SkillGauge (1);
+				Destroy (gameObject);
+				break;
+			}
+			stateTime += Time.deltaTime;
+			if (stateTime > idleStateMaxTime) {
+				stateTime = 0.0f;
+				//anim.SetBool ("Damage", false);
+				state = STATE.AFTERIDLE;
+			}
+			//state = STATE.IDLE;
+			break;
+		case STATE.DEAD:
+			state = STATE.NONE;
+			break;
                 
-                if (mHp <= 0)
-                {
-                    state = STATE.DEAD;
-                    GameManager.Instance().SkillGauge(1);
-                    Destroy(gameObject);
-                    break;
-                }
-                stateTime += Time.deltaTime;
-                if (stateTime > idleStateMaxTime)
-                {
-                    stateTime = 0.0f;
-                    state = STATE.MOVE;
-                }
-                state = STATE.IDLE;
-                break;
-            case STATE.DEAD:
-                state = STATE.NONE;
-                break;
-                
-        }
+		}
        
 		
 
 	}
 	public void Damage(int WeaponAttackPoint)
 	{
+
+		//anim.SetBool ("Damage", true);
+		stateTime = 0.0f;
         mHp -= WeaponAttackPoint;
         state = STATE.DAMAGE;
 	}
--- Assets/02.Scripts/csWeapon.cs
+++ Assets/02.Scripts/csWeapon.cs
@@ -3,21 +3,54 @@ using System.Collections;
 
 public class csWeapon : MonoBehaviour {
 
+	GameObject player;
+	int WeaponTypeNum;
+	int AttackPoint;
+	float AttackRange;
+	int killCount = 0;
+
+	public int[] WeaponStateAttack;
+	public float[] WeaponStateRange;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		UpgradeWeapon (killCount);
+		player = GameObject.Find ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	void OnTriggerEnter(Collider col)
 	{
 		if (col.gameObject.tag == "Enemy") {
-			
-			Debug.Log ("123");
+			if (player.GetComponent<csPlayerController> ().isAttack) {
+				col.gameObject.GetComponent<csEnemyRig> ().Damage(AttackPoint);
+			}
 		}
+	}
+
+	void UpgradeWeapon(int Count)
+	{
+		if (Count > 100) {
+			SetState (3);
+		} else if (Count > 60) {
+
+			SetState (2);
+		} else if (Count > 30) {
+
+			SetState (1);
+		} else {
+			SetState (0);
+		}
+	}
+
+	void SetState(int level)
+	{
+		AttackPoint = WeaponStateAttack [level];
+		AttackRange = WeaponStateRange [level];
+		transform.localScale = new Vector3 (AttackRange, AttackRange, AttackRange);
 	}
 }
