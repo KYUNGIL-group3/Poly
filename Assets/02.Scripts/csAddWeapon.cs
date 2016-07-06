3ddb047404196a640e0eee4f2dd48675dd731f35
kimkwangsik <jk140502@gmail.com>
2016-07-06 16:32:49 +0900

    Stage1 씬 삭제 스크립트 정리 프리펩 수정

---
+++ Assets/02.Scripts/csAddWeapon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class csAddWeapon : MonoBehaviour {
+
+	public GameObject[] Weapon;
+	GameObject Eweapon;
+	int WeaponNum=0;
+
+	// Use this for initialization
+	void Start () {
+		Eweapon = Instantiate (Weapon [WeaponNum], transform.position, transform.rotation) as GameObject;
+		Eweapon.transform.parent = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButtonUp ("Change")) {
+			WeaponNum++;
+			if (Weapon.Length <= WeaponNum) {
+				WeaponNum = 0;
+			}
+			Destroy (Eweapon);
+			Eweapon = Instantiate (Weapon [WeaponNum], transform.position, transform.rotation) as GameObject;
+			Eweapon.transform.parent = gameObject.transform;
+		} 
+	}
+
+}
