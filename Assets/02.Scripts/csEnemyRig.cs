1f63b96762c10104ff1a9f5120ec4e32af43a082
kimkwangsik <jk140502@gmail.com>
2016-07-08 11:48:22 +0900

    적1 모션 수정및 프리펩 수정 태그 추가

--- Assets/02.Scripts/csEnemyRig.cs
+++ Assets/02.Scripts/csEnemyRig.cs
@@ -15,13 +15,18 @@ public class csEnemyRig : MonoBehaviour {
     }
     STATE state = STATE.IDLE;
     float stateTime = 0.0f;
-
+	Transform[] obj;
     public float idleStateMaxTime = 1.0f;   //대기시간,경직시간
     public float attackableRange = 2.0f;    //공격범위
     public float attackStateMaxTime = 4.0f; //공격대기시간
     public int monsterAttackPoint;  //몬스터 공격력
     public float checkAttackDistance = 2.0f; //견제공격 범위
 
+	float rotspeed = 5.0f;
+	float rottime = 0.0f;
+	public float rotmaxtime = 3.0f;
+	bool attackMotion = false;
+	bool isattackMotion = true;
 
     Transform player;
     public float speed;
@@ -34,41 +39,49 @@ public class csEnemyRig : MonoBehaviour {
 	void Start () {
 		player = GameObject.FindWithTag("CharCenter").transform;
 		enemyController = GetComponent<CharacterController> ();
+		obj = gameObject.GetComponentsInChildren<Transform> ();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		//Debug.Log (state);
+		for (int i = 1; i < obj.Length; i++) {
+			if (obj [i].tag == "Body") {
+				obj [i].transform.Rotate (new Vector3 (0.0f, rotspeed, 0.0f));
+			}
+		}
+
 		switch (state) {
-		    case STATE.IDLE:
-                distance = Vector3.Distance(transform.position, player.position);
-                stateTime += Time.deltaTime;
-                if (distance <= 5.0f)
-                {
-                    state = STATE.MOVE;
-                    Debug.Log("Move로 이동");
-                    stateTime = 0.0f;
-                }
-                if (stateTime > idleStateMaxTime) {
+		case STATE.IDLE:
+			distance = Vector3.Distance (transform.position, player.position);
+			stateTime += Time.deltaTime;
+
+			if (distance < attackableRange) {
+				//anim.SetBool ("enemyIsAttack", true);
+				state = STATE.ATTACK;
+
+				stateTime = 0.0f;
+				Debug.Log ("Attack으로 이동");
+			} else if (distance <= 5.0f) {
+				state = STATE.MOVE;
+				Debug.Log ("Move로 이동");
+				stateTime = 0.0f;
+			}
+
+			if (stateTime > idleStateMaxTime) {
 				
 				stateTime = 0.0f;
 
                    
-                    if (distance < attackableRange)
-                    {
-                        //anim.SetBool ("enemyIsAttack", true);
-                        state = STATE.ATTACK;
-                        Debug.Log("Attack으로 이동");
-                    }
+                  
 
-                    //if (distance < checkAttackDistance) {
-                    //state = STATE.CHECKATTACK;
-                    //}
-                }
+				//if (distance < checkAttackDistance) {
+				//state = STATE.CHECKATTACK;
+				//}
+			}
 
-            break;
+			break;
 
-            case STATE.AFTERIDLE:
+		case STATE.AFTERIDLE:
 			stateTime += Time.deltaTime;
 			if (stateTime > idleStateMaxTime) {
 				stateTime = 0.0f;
@@ -76,40 +89,39 @@ public class csEnemyRig : MonoBehaviour {
 			}
 			break;
 
-            case STATE.MOVE:
+		case STATE.MOVE:
                 //해당 스크립트 오브젝트와 player간의 거리
 			distance = Vector3.Distance (transform.position, player.position);
 
 			if (distance < attackableRange) {
 				state = STATE.IDLE;
 				//stateTime = 0.0f;
-                    Debug.Log("Idle이동");
-			}
-                else {
+				Debug.Log ("Idle이동");
+			} else {
 				Vector3 dir = player.position - transform.position;
 				dir.y = 0.0f;
 				dir.Normalize ();
 				enemyController.SimpleMove (dir * speed);
-                    Debug.Log("추적");
-            }
+				Debug.Log ("추적");
+			}
 			break;
 
-            case STATE.ATTACK:
-                distance = Vector3.Distance(transform.position, player.position);
-                stateTime += Time.deltaTime;
+		case STATE.ATTACK:
+			distance = Vector3.Distance (transform.position, player.position);
+			stateTime += Time.deltaTime;
                 
-                if (stateTime > attackStateMaxTime) {
+			if (stateTime > attackStateMaxTime) {
 				stateTime = 0.0f;
 				GameManager.Instance ().PlayerHealth (monsterAttackPoint);
-				Debug.Log("Attack");
-                    //anim.SetBool ("enemyIsAttack", false);
-                    state = STATE.IDLE;
-                }
+				Debug.Log ("Attack");
+				//anim.SetBool ("enemyIsAttack", false);
+				state = STATE.IDLE;
+			}
                
                 
 			break;
 
-            case STATE.DAMAGE:
+		case STATE.DAMAGE:
 
 			if (mHp <= 0) {
 				state = STATE.DEAD;
@@ -126,7 +138,7 @@ public class csEnemyRig : MonoBehaviour {
 			//state = STATE.IDLE;
 			break;
 
-            case STATE.DEAD:
+		case STATE.DEAD:
 			state = STATE.NONE;
 			break;
                 
@@ -135,6 +147,22 @@ public class csEnemyRig : MonoBehaviour {
 		
 
 	}
+
+	void FixedUpdate () {
+		rottime += Time.deltaTime;
+		if (isattackMotion) {
+			for (int i = 1; i < obj.Length; i++) {
+				if (obj [i].tag == "EnemyDown") {
+					obj [i].transform.Rotate (new Vector3 (rotspeed * 0.5f, 0.0f, 0.0f));
+				}
+			}
+		}
+		if (rottime > rotmaxtime) {
+			rottime = 0.0f;
+			isattackMotion = false;
+		}
+	}
+
 	public void Damage(int WeaponAttackPoint)
 	{
 
@@ -143,4 +171,13 @@ public class csEnemyRig : MonoBehaviour {
         mHp -= WeaponAttackPoint;
         state = STATE.DAMAGE;
 	}
+
+//	IEnumerable AttackMotion()
+//	{
+//		for (int i = 1; i < obj.Length; i++) {
+//			if (obj [i].tag == "EnemyDown") {
+//				obj [i].transform.Rotate (new Vector3 (rotspeed *0.1f, 0.0f, 0.0f));
+//			}
+//		}
+//	}
 }
