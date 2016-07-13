029fdc70f6bbb62f46cabd83b1533331c99b13df
서태덕 <sikong32@naver.com>
2016-07-13 13:45:22 +0900

    UI(미완성) 원거리 투사체 네임드 투사체 각종 오브젝트

---
+++ Assets/UI/오브젝트/csmove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csmove : MonoBehaviour {
+	// Use this for initialization
+	Transform obj = null;
+	public float speed = 500.0f; 
+	void Start () 
+	{
+		obj = GameObject.Find ("Cube").transform;
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.RotateAround(obj.position, Vector3.up, speed * Time.deltaTime);
+		//transform.LookAt (obj);
+
+
+	}
+}
