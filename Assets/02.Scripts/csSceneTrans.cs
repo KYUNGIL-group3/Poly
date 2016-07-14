2d81fc96fe1a1056f89fcc707d55930dc57383e4
InJaeMyung <dlswo555@gmail.com>
2016-07-14 17:04:43 +0900

    몬스터 타고 올라가는 현상 수정 플레이어 화살에 firePos 추가 씬 트랜스 씬 추가

---
+++ Assets/02.Scripts/csSceneTrans.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csSceneTrans : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void StartButton()
+    {
+
+    }
+    public void ShopButton()
+    {
+
+    }
+    public void StageButton()
+    {
+
+    }
+    public void ItemButton()
+    {
+
+    }
+    public void ProfileButton()
+    {
+
+    }
+
+}
