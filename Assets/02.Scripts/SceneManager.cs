612a366415e628235d15c3416aebfe8d1aa704e1
kimkwangsik <jk140502@gmail.com>
2016-07-18 15:21:03 +0900

    도시 맵 생성 반투명화 적용 벽생성 필요

---
+++ Assets/02.Scripts/SceneManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+	static SceneManager _instance = null;
+
+	public static SceneManager Instance()
+	{
+		return _instance;
+	}
+
+	void Awake()
+	{
+		if (_instance == null)
+		{
+			_instance = this;
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartButton()
+    {
+		Application.LoadLevel ("POLY_ROBY");
+    }
+    public void ShopButton()
+    {
+
+    }
+    public void StageButton()
+    {
+		Application.LoadLevel ("Stage1-Happy");
+    }
+    public void ItemButton()
+    {
+
+    }
+    public void ProfileButton()
+    {
+		Application.LoadLevel ("POLY_Profile");
+    }
+
+}
