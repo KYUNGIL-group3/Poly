561b146b3e1a5bef1ccb9cd8657464a7dd985cff
kimkwangsik <jk140502@gmail.com>
2016-07-19 14:32:28 +0900

    타임스케일 수정중(스킬 쓰지 말것)

--- Assets/02.Scripts/csPointSetPosition.cs
+++ Assets/02.Scripts/csPointSetPosition.cs
@@ -3,20 +3,43 @@ using System.Collections;
 
 public class csPointSetPosition : MonoBehaviour {
 
+	public GameObject pointsprite;
 	float Cdis;
 	float Fdis;
 	float Bdis;
 	float Ldis;
 	float Rdis;
-
+	float del = 0.0f;
+	bool once = false;
 	// Use this for initialization
 	void Start () {
 		
+
+
+		//StartCoroutine (setNextPoint ());
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (once) {
+			return;
+		}
+		if (del > 0.1f) {
+			once = true;
+			RaycastHit hit;
+
+			if (Physics.Raycast (transform.position, Vector3.forward, out hit, 1.5f, 13 << 13)) {
+			
+			} else {
+				GameObject goTemp = Instantiate (pointsprite, transform.position + Vector3.forward * 1.0f,
+					                    Quaternion.Euler (new Vector3 (0.0f, 0.0f, 0.0f))) as GameObject;
+				goTemp.transform.parent = gameObject.transform.parent;
+			}
+		} else {
+			del += Time.unscaledDeltaTime;
+		}
+
 	}
 
 	public void Centerdis(float i)
@@ -44,5 +67,6 @@ public class csPointSetPosition : MonoBehaviour {
 	{
 		Bdis = i;
 	}
+
 
 }
--- Assets/02.Scripts/csSkillAction.cs
+++ Assets/02.Scripts/csSkillAction.cs
@@ -18,31 +18,40 @@ public class csSkillAction : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		SkillManager = GameObject.Find ("TimeManager");
+		playerPos = SkillManager.transform.position + new Vector3(0.0f, 0.0f, 0.0f);
+//		playerPos = SkillManager.transform.position + new Vector3(-10.0f, 0.0f, -10.0f);
+//		
+//
+//		moveplayerPos = playerPos;
+//
+//		for (int x = 0; x < 20; x++) {
+//			for (int z = 0; z < 20; z++) {
+//				GameObject goTemp = Instantiate (pointsprite, moveplayerPos,
+//					                       Quaternion.Euler (new Vector3 (0.0f, 0.0f, 0.0f))) as GameObject;
+//				goTemp.transform.parent = gameObject.transform;
+//
+//				if (z == 10 && x == 10) {
+//					SkillManager.GetComponent<csTimeManager> ().FirstPoint (goTemp);
+//					Transform[] colorTemp = goTemp.GetComponentsInChildren<Transform> ();
+//					//Debug.Log (colorTemp[1].gameObject.name);
+//					colorTemp[1].gameObject.GetComponent<SpriteRenderer> ().color = new Color (0.0f, 255.0f, 255.0f, 255.0f);
+//					//goTemp.GetComponent<SpriteRenderer> ().color = new Color (0.0f, 255.0f, 255.0f, 255.0f);
+//				}
+//			
+//				moveplayerPos += Vector3.forward * 1.0f;
+//			}
+//			playerPos += Vector3.right * 1.0f;
+//			moveplayerPos = playerPos;
+//		} 
 
-		playerPos = SkillManager.transform.position + new Vector3(-10.0f, 0.0f, -10.0f);
-		//playerPos = SkillManager.transform.position + new Vector3(0.0f, 0.0f, 0.0f);
 
-		moveplayerPos = playerPos;
+		GameObject goTemp = Instantiate (pointsprite, playerPos,
+			                    Quaternion.Euler (new Vector3 (0.0f, 0.0f, 0.0f))) as GameObject;
+		goTemp.transform.parent = gameObject.transform;
+		SkillManager.GetComponent<csTimeManager> ().FirstPoint (goTemp);
+		Transform[] colorTemp = goTemp.GetComponentsInChildren<Transform> ();
+		colorTemp[1].gameObject.GetComponent<SpriteRenderer> ().color = new Color (0.0f, 255.0f, 255.0f, 255.0f);
 
-		for (int x = 0; x < 20; x++) {
-			for (int z = 0; z < 20; z++) {
-				GameObject goTemp = Instantiate (pointsprite, moveplayerPos,
-					                       Quaternion.Euler (new Vector3 (0.0f, 0.0f, 0.0f))) as GameObject;
-				goTemp.transform.parent = gameObject.transform;
-
-				if (z == 10 && x == 10) {
-					SkillManager.GetComponent<csTimeManager> ().FirstPoint (goTemp);
-					Transform[] colorTemp = goTemp.GetComponentsInChildren<Transform> ();
-					//Debug.Log (colorTemp[1].gameObject.name);
-					colorTemp[1].gameObject.GetComponent<SpriteRenderer> ().color = new Color (0.0f, 255.0f, 255.0f, 255.0f);
-					//goTemp.GetComponent<SpriteRenderer> ().color = new Color (0.0f, 255.0f, 255.0f, 255.0f);
-				}
-			
-				moveplayerPos += Vector3.forward * 1.0f;
-			}
-			playerPos += Vector3.right * 1.0f;
-			moveplayerPos = playerPos;
-		} 
 
 	}
 
