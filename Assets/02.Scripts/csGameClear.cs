96e68dc2bbd8117069faf61c9e71dd1787aaa04d
kimkwangsik <jk140502@gmail.com>
2016-07-20 16:51:45 +0900

    스테이지 4 스포트라이트 추가

--- Assets/02.Scripts/csGameClear.cs
+++ Assets/02.Scripts/csGameClear.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class csGameClear : MonoBehaviour {
-
+	public GameObject Boss;
+	public GameObject AllSpwanPoint;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +11,9 @@ public class csGameClear : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!Boss) {
+
+
+		}
 	}
 }
